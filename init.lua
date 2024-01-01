cb4f63357858eada44c7db2e8e80a48d57859f90
TingSpace <ting.space@outlook.com>
2024-01-02 04:37:30 +1100

    Install lazy.nvim as package manager

--- init.lua
+++ init.lua
@@ -1,1 +1,63 @@
 require("tingspace")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.5",
+        dependencies = { "nvim-lua/plenary.nvim" },
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+    },
+    "ThePrimeagen/harpoon",
+    "mbbill/undotree",
+    "tpope/vim-fugitive",
+    "m4xshen/autoclose.nvim",
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
+    {
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v3.x",
+        lazy = true,
+        config = false,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            { "hrsh7th/cmp-nvim-lsp" },
+        },
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            { "L3MON4D3/LuaSnip" }
+        },
+    },
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+}
+
+require("lazy").setup(plugins)
+
