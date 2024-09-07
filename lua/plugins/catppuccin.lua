8870a016cc5062dd519710fce4760901b39c6caa
TingSpace <ting.space@outlook.com>
2024-09-07 17:09:31 +1000

    Use Gruvbox dark as the default theme

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,7 +4,7 @@ return {
         name = "catppuccin",
         priority = 1000,
         config = function()
-            vim.cmd.colorscheme("catppuccin")
+            -- vim.cmd.colorscheme("catppuccin")
 
             vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
             vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
