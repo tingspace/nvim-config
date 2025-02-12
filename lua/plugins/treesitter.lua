29b892f3e02543b78a187ebc429270b78fbcb299
Hayden Bevan <hayden.bevan@myob.com>
2025-02-12 20:18:58 +1100

    Pin Treesitter plugin version to current stable

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
+        commit = "49f1b9a",
         config = function()
             require'nvim-treesitter.configs'.setup {
                 ensure_installed = {
